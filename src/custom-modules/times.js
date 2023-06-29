6130b2d0788263b9ac1cba0d75592e5b70e06c98
Muhammad Rivandra <rivandra0@gmail.com>
2023-06-29 08:40:15 +0700

    testing the git

--- src/custom-modules/times.js
+++ src/custom-modules/times.js
@@ -4,6 +4,7 @@ function getDifferenceInSecond (dateTime1, dateTime2) {
 	const differenceInMs = dateTime2 - dateTime1
 	const differenceInSeconds = Math.floor((dateTime1 - dateTime2)/1000)
 	return Math.abs(differenceInSeconds)
+	console.log("testing")
 }
 
 export { getDifferenceInSecond }
